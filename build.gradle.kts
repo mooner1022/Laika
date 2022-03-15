1c46e3b82e5d4502559fb7f0c4fcf0ec591bc1ae
mooner1022 <moonmk10221@gmail.com>
2022-03-15 20:21:59 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,13 @@
-import org.jetbrains.dokka.gradle.DokkaTask
-
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
-val atomicfu_version = "0.17.1"
-
 plugins {
     `java-library`
-    kotlin("jvm") version "1.6.10" apply false
+    `maven-publish`
+    kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
     id("org.jetbrains.dokka") version "1.6.10"
-    id("maven-publish")
 }
 
 group = "dev.mooner"
@@ -24,6 +20,8 @@ repositories {
 dependencies {
     dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.10")
 
+    implementation(kotlin("stdlib"))
+
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-cio:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
@@ -31,4 +29,23 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    //classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.mooner.laika"
+            artifactId = "Laika"
+            version = "0.0.1"
+
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
 }
